0e3aab822dd63327c8496feae3f7b13dbb8fce5e
johnatanga <jattanga@hotmail.com>
2020-12-12 22:43:55 -0500

    cachesolve calculates the inverse of the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,31 @@
-makeCacheMatrix <- function(x = matrix()) {
-  i <- NULL
+## The function below is used to check the cache for inverse  of matrix
+## If not found then one should compute the inverse using solve()
+
+## The function uses Lexical scoping
+## It should return  a list of four functions set, get, setinverse and get inverse.
+
+makeCacheMatrix <-  function(x = matrix()) {
+  
+  i <-  NULL
   
   set <- function(y){
     x <<- y
-    i <<- NULL  
+    i <<- NULL
     
   }
   get <- function() x
-  setinverse <- function(inverse) i <<- inverse 
-  getinveser <- function() i
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
   list(set = set,
        get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
+##  Thus function caculates the inverse of the matrix but first checks cache should there be a code
 
 cacheSolve <- function(x, ...) {
+  
   i <- x$getinverse()
   if  (!is.null(i)) {
     message("getting cached data")
@@ -25,5 +34,6 @@ cacheSolve <- function(x, ...) {
   data <- x$get()
   i <- solve(data, ...)
   x$setinverse(i)
+  i
   
 }
